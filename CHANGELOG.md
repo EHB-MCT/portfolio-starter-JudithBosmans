9c131629c336cb0a8d52b407d5d76807a0a2ec06
Judith Bosmans <judith.bosmans@student.ehb.be>
2023-11-23 12:41:44 +0100

    add README, CODE_OF_CONDUCT, CONTRIBUTION, CHANG..

---
+++ CHANGELOG.md
@@ -0,0 +1,33 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [Unreleased]
+
+### Added
+
+- New features that have been added since the last release
+
+### Changed
+
+- Changes in existing functionality since the last release
+
+### Deprecated
+
+- Features that will be removed in upcoming releases
+
+### Removed
+
+- Features that have been removed
+
+### Fixed
+
+- Any bug fixes since the last release
+
+### Security
+
+- Any security updates since the last release
+
+### Fixed
+
+- Bug fix related to ...
--- images/api/src/db/migrations/20231108201357_colors.js
+++ images/api/src/db/migrations/20231108201357_colors.js
@@ -3,18 +3,23 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable("colors", function (table) {
-    table.increments("id");
-    table.string("name");
-    table.integer("year");
-  });
+  return knex.schema
+    .createTable("artists", function (table) {
+      table.increments("id").primary();
+      table.string("name").notNullable();
+      table.string("age");
+    })
+    .createTable("works", function (table) {
+      table.increments("id").primary();
+      table.string("name").notNullable();
+      table.integer("artist_id").unsigned().references("id").inTable("artists");
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-
 exports.down = function (knex) {
-  return knex.schema.dropTable("colors");
+  return knex.schema.dropTableIfExists("works").dropTableIfExists("artists");
 };
